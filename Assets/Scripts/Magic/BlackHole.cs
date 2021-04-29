a183b89ed3fa16c68efeb98af888127dfa273236
Maksim Tretyakov <m.tretyakov@kefirgames.com>
2021-04-30 01:46:43 +0300

    end two day

--- Assets/Scripts/Magic/BlackHole.cs
+++ Assets/Scripts/Magic/BlackHole.cs
@@ -4,17 +4,17 @@ public class BlackHole : MonoBehaviour
 {
     [SerializeField] private MagicInfo magicInfo;
     [SerializeField] private LayerMask layerMask;
-    
+
     private void Update()
     {
         var enemy = Physics.OverlapSphere(transform.position, magicInfo.ExplosionRange,layerMask);
         foreach (var obj in enemy)
         {
             if (obj.GetComponent<Rigidbody>()!=null)
-            {
+            {//пофиксить кружение
                 var position = obj.gameObject.transform.position;
 
-                obj.gameObject.GetComponent<Rigidbody>().AddForce((transform.position -position).normalized * 40f);
+                obj.gameObject.GetComponent<Rigidbody>().AddForce((transform.position -position).normalized * 35f);
             }
         }
     }
